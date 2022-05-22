eb772d4a1f9c91238ea0cab7be9ababd8518aeca
Yigithan Kadir BALCI <yigithanbalci@gmail.com>
2022-05-22 22:39:18 +0200

    Initial commit

---
+++ src/main/java/com/example/articlemanagement/ArticleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.articlemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/articlemanagement/ArticleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.articlemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
